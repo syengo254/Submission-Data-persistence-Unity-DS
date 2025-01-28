ee258dfc7a0c8b7e99a923dffd5c374db4fee3e0
David Syengo <david.syengo019@gmail.com>
2025-01-28 23:11:42 +0300

    wip: ability to toggle music on and off

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class SettingsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static SettingsManager Instance {get; private set;}
+    public bool musicOn = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Awake() {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/SettingsUIHandler.cs
+++ Assets/Scripts/SettingsUIHandler.cs
@@ -7,15 +7,23 @@ using UnityEngine.UI;
 public class SettingsUIHandler : MonoBehaviour
 {
     [SerializeField] Button clearSettingsBtn;
+    [SerializeField] Toggle musicToggle;
 
     private void Start() {
         clearSettingsBtn.onClick.AddListener(() => {
             GameManager.Instance.ClearSavedScores();
         });
+
+        musicToggle.isOn = SettingsManager.Instance.musicOn;
     }
 
     public void LoadStartMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void ToggleGameMusic()
+    {
+        SettingsManager.Instance.musicOn = musicToggle.isOn;
     }
 }
